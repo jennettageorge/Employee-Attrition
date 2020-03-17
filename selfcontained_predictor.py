77d4a9b6a74adc48e9d8ab2c80dab0b448b61699
jennettageorge <jennageorge89@me.com>
2020-03-16 21:10:30 -0400

    added py file

---
+++ selfcontained_predictor.py
@@ -0,0 +1,181 @@
+import pandas as pd
+import numpy as np
+import datetime
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score, recall_score, roc_auc_score, roc_curve
+from sklearn.metrics import confusion_matrix
+import itertools
+from sklearn import preprocessing
+from sklearn.model_selection import GridSearchCV
+
+
+employees = pd.read_csv('data/Employees.csv')
+surveys = pd.read_csv('data/Survey.csv')
+performance = pd.read_csv('data/Performance.csv')
+
+
+
+'''function to turn a datetime pd.Series into a numerical feature'''
+def date_to_feat(series):
+    dtseries = series.apply(lambda x : datetime.datetime.strptime(x, '%m/%d/%y'))
+    epoch = datetime.datetime.utcfromtimestamp(0)
+    featseries = dtseries.apply(lambda x : (x - epoch).total_seconds()/86400)
+    return featseries
+
+
+def clean_data(employees,surveys,performance):
+    #Employees Table
+
+    #Deal with null values
+    employees.EmploymentEndReason = employees.EmploymentEndReason.apply(lambda x: 0 if str(x) == 'nan' else 1)
+    employees.NumPreviousCompanies = employees.NumPreviousCompanies.fillna(0)
+
+    #Deal with Dates
+    employees['EmploymentStartTime'] = date_to_feat(employees.EmploymentStartDate)
+    employees.drop('EmploymentStartDate', axis=1,inplace=True)
+    employees.drop('EmploymentEndDate',axis=1,inplace=True)
+
+    performance['ReviewTime'] = date_to_feat(performance.ReviewDate)
+    performance.drop('ReviewDate', axis=1, inplace=True)
+
+
+    #Deal with categorical fields
+    is_categorical = employees[['DegreeCompleted','DegreeField','Department','Gender','MaritalStatus','TravelFrequency']]
+
+    cat_dummies = pd.get_dummies(is_categorical)
+
+    employees = employees.join(cat_dummies)
+    for col in list(is_categorical.columns):
+        employees = employees.drop(col, axis=1)
+
+    #Survey Table
+    # Cleaning the survey data before we join tables
+    surveys.Response = surveys.Response.apply(lambda x: 0 if (x == 'Very Unsatisfied' or x =='Very Poor')\
+                                  else (1 if (x == 'Somewhat Unsatisfied' or x == 'Poor')\
+                                  else (2 if (x == 'Neither Satisfied nor Unsatisfied' or x == 'Fair')\
+                                  else (3 if (x == 'Somewhat Satisfied' or x == 'Good')\
+                                  else (4 if (x == 'Very Satisfied' or x =='Excellent') else 5)))))
+    Q1 = surveys[surveys.QuestionNum == 'Q1']
+    Q2 = surveys[surveys.QuestionNum == 'Q2']
+    Q3 = surveys[surveys.QuestionNum == 'Q3']
+    Q4 = surveys[surveys.QuestionNum == 'Q4']
+
+    Q1.rename(columns={"Response":"ResponseQ1"}, inplace = True)
+    Q2.rename(columns={"Response":"ResponseQ2"}, inplace = True)
+    Q3.rename(columns={"Response":"ResponseQ3"}, inplace = True)
+    Q4.rename(columns={"Response":"ResponseQ4"}, inplace = True)
+
+    Q1 = Q1.set_index(employees.EmployeeId).drop('EmployeeId',axis=1)
+    Q2 = Q2.set_index(employees.EmployeeId).drop('EmployeeId',axis=1)
+    Q3 = Q3.set_index(employees.EmployeeId).drop('EmployeeId',axis=1)
+    Q4 = Q4.set_index(employees.EmployeeId).drop('EmployeeId',axis=1)
+
+    largetable = pd.concat([Q1,Q2,Q3,Q4], sort=False)
+    responses = largetable.groupby('EmployeeId').sum()
+    responses.head()
+
+    employees = employees.set_index('EmployeeId')
+    emp = employees.join(responses)
+    emp = emp.join(performance.iloc[:,1:])
+    labels = emp.EmploymentEndReason
+    emp.drop('EmploymentEndReason', axis=1,inplace=True)
+    return emp, labels
+
+
+def calculate_vif_(X, thresh=5.0):
+    variables = list(range(X.shape[1]))
+    dropped = True
+    while dropped:
+        dropped = False
+        vif = [variance_inflation_factor(X.iloc[:, variables].values, ix)
+               for ix in range(X.iloc[:, variables].shape[1])]
+
+        maxloc = vif.index(max(vif))
+        if max(vif) > thresh:
+            print('dropping \'' + X.iloc[:, variables].columns[maxloc] +
+                  '\' at index: ' + str(maxloc))
+            del variables[maxloc]
+            dropped = True
+
+    print('Remaining variables:')
+    print(X.columns[variables])
+    return X.iloc[:, variables]
+
+def calculate_corr_(X):
+    corr = X.corr()
+    colsbefore = set(X.columns)
+    columns = np.full((corr.shape[0],), True, dtype=bool)
+    for i in range(corr.shape[0]):
+        for j in range(i+1, corr.shape[0]):
+            if corr.iloc[i,j] >= 0.8:
+                if columns[j]:
+                    columns[j] = False
+    selected_columns = X.columns[columns]
+    X = X[selected_columns]
+    colsafter = set(X.columns)
+
+    print('# of Columns after removal: ', len(list(X.columns)))
+    print('Columns Removed: ', colsbefore - colsafter)
+
+
+    return X[selected_columns]
+
+'''def predict(data, label):
+    #train test split
+    x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, stratify=labels, random_state=0)
+
+    # Train A Decision Tree Model
+    # Create decision tree classifer object
+    clf = RandomForestClassifier(random_state=0, n_jobs=-1)
+
+    # Train model
+    model = clf.fit(x_train, y_train)
+
+    #Predict on testing data
+    rf_predictions = clf.predict(x_test)
+
+    # Evaluate predictions
+    accuracy = accuracy_score(y_test, rf_predictions)
+    print("Accuracy: %.2f%%" % (accuracy * 100.0))
+    cm = confusion_matrix(y_test, rf_predictions)
+    print('Confusion Matrix: \n' , cm)
+    return rf_predictions
+'''
+def predict(data,label):
+    #train test split
+    x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, stratify=labels, random_state=0)
+
+    # Create the parameter grid based on the results of random search
+    param_grid = {
+        'bootstrap': [True],
+        'max_depth': [30,40,50, 60],
+        'max_features': [2, 3, 4],
+        'min_samples_leaf': [4, 5],
+        'min_samples_split': [8, 10, 12],
+        'n_estimators': [50,100, 200, 300]
+    }
+    # Create a based model
+    rf = RandomForestClassifier()
+    # Instantiate the grid search model
+    grid_search = GridSearchCV(estimator = rf, param_grid = param_grid,
+                              cv = 3, n_jobs = -1, verbose = 2)
+    # Fit the grid search to the data
+    grid_search.fit(x_train, y_train)
+    grid_search.best_params_
+    best_grid = grid_search.best_estimator_
+    print(best_grid)
+    predictions = best_grid.predict(x_test)
+
+    # evaluate predictions
+    accuracy = accuracy_score(y_test, predictions)
+    print("Accuracy: %.2f%%" % (accuracy * 100.0))
+    return predictions
+
+if __name__ == '__main__':
+    data, labels = clean_data(employees,surveys,performance)
+    data = calculate_vif_(data)
+    data = calculate_corr_(data)
+    predict(data,labels)
